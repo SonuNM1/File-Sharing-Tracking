088edb5a154be7d746fe38cc192890fcbec5ec4d
Sonu N Mahto <sonu.mahto362000@gmail.com>
2024-08-04 19:29:28 +0530

    4 August

---
+++ Frontend/index.js
@@ -0,0 +1,61 @@
+
+const dropZone = document.querySelector(".drop-zone") ; 
+const fileInput = document.querySelector("#fileInput") ; 
+const browseBtn = document.querySelector(".browseBtn") ; 
+
+const host = "https://innShare.herokuapp.com/" ; 
+const uploadURL = host + `${api/files}` ;
+
+
+dropZone.addEventListener("dragover", (e)=>{
+    e.preventDefault() ; 
+    console.log("dragging");
+
+    if(!dropZone.classList.contains("dragged")){
+        dropZone.classList.add("dragged") ; 
+    }   
+}) ;
+
+dropZone.addEventListener("dragleave", ()=>{
+    dropZone.classList.remove("dragged") ; 
+})
+
+dropZone.addEventListener("drop", (e)=>{
+    e.preventDefault();
+    dropZone.classList.remove("dragged") ; 
+    const files = e.dataTransfer.files;
+    console.log(files);
+    
+    if(files.length){
+        fileInput.files = files ; 
+        uploadFile() ;
+    }
+})
+
+fileInput.addEventListener("change", ()=>{
+    uploadFile() ; 
+})
+
+browseBtn.addEventListener("click", ()=>{
+    fileInput.click() ; 
+})
+
+const uploadFile = ()=>{
+
+    const file = fileInput.files[0] ; 
+    const formData = new formData() ;
+    formData.append("myFile", file) ; 
+
+    const xhr = new XMLHttpRequest() ; 
+
+    xhr.onreadystatechange = ()=>{
+        if(xhr.readyState === XMLHttpRequest.DONE){
+            console.log(xhr.response);
+            
+        }
+    }
+
+    xhr.open("POST", uploadURL) ;
+
+    xhr.send(formData)
+}
