4763f4456d97c1de43e7c943b54f03e2b4b7df3d
Sonu N Mahto <sonu.mahto362000@gmail.com>
2024-08-07 23:37:37 +0530

    7 August

--- Frontend/index.js
+++ Frontend/index.js
@@ -3,6 +3,15 @@ const dropZone = document.querySelector(".drop-zone") ;
 const fileInput = document.querySelector("#fileInput") ; 
 const browseBtn = document.querySelector(".browseBtn") ; 
 
+const progressContainer = document.querySelector(".progress-container") ; 
+const bgProgress = document.querySelector(".bg-progress") ;
+const progressBar = document.querySelector(".progress-bar") ; 
+const percentDiv = document.querySelector("#percent") ; 
+
+const sharingContainer = document.querySelector(".sharing-container") ; 
+const fileURLInput = document.querySelector("#fileURL") ; 
+const copyBtn = document.querySelector("#copyBtn") ; 
+
 const host = "https://innShare.herokuapp.com/" ; 
 const uploadURL = host + `${api/files}` ;
 
@@ -40,7 +49,14 @@ browseBtn.addEventListener("click", ()=>{
     fileInput.click() ; 
 })
 
+copyBtn.addEventListener("click", ()=>{
+    fileURLInput.select() ; 
+    document.execCommand("copy") ; 
+})
+
 const uploadFile = ()=>{
+
+    progressContainer.style.display = "block" ; 
 
     const file = fileInput.files[0] ; 
     const formData = new formData() ;
@@ -51,11 +67,40 @@ const uploadFile = ()=>{
     xhr.onreadystatechange = ()=>{
         if(xhr.readyState === XMLHttpRequest.DONE){
             console.log(xhr.response);
-            
+
+            showLink(JSON.parse(xhr.response)) ; 
         }
     }
+
+    xhr.upload.onprogress = updateProgress ; 
 
     xhr.open("POST", uploadURL) ;
 
     xhr.send(formData)
 }
+
+
+const updateProgress = (e)=>{
+
+    const percent = Math.round((e.loaded / e.total) * 100) ; 
+    // console.log(percent);
+
+    bgProgress.computedStyleMap.width = `${percent}%`; 
+
+    percentDiv.innerHTML = percent ; 
+
+    progressBar.style.transform = `scaleX(${percent/100}%)` ; 
+    
+}
+
+const showLink =({file: url})=>{
+    console.log(url);
+    progressContainer.style.display = "none" ; 
+    progressContainer.style.display = "none" ; 
+    sharingContainer.style.display = "block" ; 
+
+    fileURLInput.value = url ; 
+}
+
+
+
