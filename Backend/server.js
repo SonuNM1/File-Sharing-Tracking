966a54387a4b65babda0cab8441d29fe2d051b21
Sonu N Mahto <sonu.mahto362000@gmail.com>
2024-08-12 23:06:41 +0530

    resolving cors error

--- Backend/server.js
+++ Backend/server.js
@@ -17,7 +17,7 @@ const corsOptions = {
 */
 
 const corsOptions ={
-  origin: ['http://127.0.0.1:5500','https://syncshare.netlify.app/']
+  origin: ['http://127.0.0.1:5500','https://syncshare.netlify.app']
 }
 
 
